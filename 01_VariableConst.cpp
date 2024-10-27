05f4c88308f1264fe38605eadb734bb24edbda8e
MOhitambika7 <mohitambikanand7@gmail.com>
2024-10-27 13:49:09 +0545

    This is the code that explains about the Variable and constants.

---
+++ 01_VariableConst.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int bankBalance;
+    bankBalance = 1500000000;
+    cout << "My bank balance is NRS. " << bankBalance << endl;
+    return 0;
+}
