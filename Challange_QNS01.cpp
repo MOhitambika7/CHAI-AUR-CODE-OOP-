1046e0a2025611316da8ddd88e8901f631cc5640
MOhitambika7 <mohitambikanand7@gmail.com>
2024-10-31 12:56:37 +0545

    This is the first task/qns problem practice for C++.

---
+++ Challange_QNS01.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+   string teaType = "Masala tea";
+   float teaPrice = 44.99; // Price per kilogram.
+   char teaRating = 'A';
+
+   cout << "The type of tea is:- " << teaType << "\n";
+   cout << "The price of the tea per kilogram is:-" << teaPrice << "\n";
+   cout << "The rating for the tea is:-" << teaRating << "\n";
+   return 0;
+}
