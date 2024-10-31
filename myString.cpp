12d67a3ef71fdea1f5f7466be12f6062b88e9633
MOhitambika7 <mohitambikanand7@gmail.com>
2024-10-31 11:57:30 +0545

    This is the C++ code for using the string headerfile and how it operates the codes in terminal

---
+++ myString.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string favouriteTea = "Lemon Tea";
+    string Description = "Knowm as \"best\"  tea";
+    cout << favouriteTea << endl;
+    cout << Description << endl;
+    return 0;
+}
